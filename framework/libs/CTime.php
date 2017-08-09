1ddd1185bbeea4a4524497d5d75270a99951399a
ctymtce <ctymtce@126.com>
2017-08-09 16:52:47 +0800

    added weekDates

--- framework/libs/CTime.php
+++ framework/libs/CTime.php
@@ -40,6 +40,16 @@ class CTime {
         $week_last_end   = date('Y-m-d', strtotime($week_last_first)+604800);
         return array($week_last_first, $week_last_end);
     }
+    //获取某年某周的开始日期和结果日期(return array)
+    static function weekDates($wk, $year=null)
+    {
+        $wk = intval($wk);
+        if(!$year) $year = date('Y');
+        $year_start = "{$year}-01-01";
+        $date_first = date('Y-m-d', strtotime($year_start)+(($wk*7)*86400));
+        $date_end = date('Y-m-d', strtotime($year_start)+((($wk+1)*7)*86400));
+        return array($date_first, $date_end);
+    }
 
     //指定日期的距离n天
     static function dayDiff($dtime=null, $nd=0)
